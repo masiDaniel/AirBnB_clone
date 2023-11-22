964b7946d963ddb576ec74b95df5a35267531758
azte-c <masidaniel02@gmail.com>
2023-11-22 17:02:08 +0300

    base model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,20 @@ time_form = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel():
     """base model class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initializing the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated at":
+                    setattr(self, key, datetime.strptime(value, time_form))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """string rep of class"""
