d90f115eb83e9e053b1cb96be4202d225e7c5396
masiDaniel <masidaniel02@gmail.com>
2024-03-15 21:42:21 +0300

    reload method and base

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""module to define basemodel class"""
 
 import uuid
 import models
@@ -8,17 +9,18 @@ time_format = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 class BaseModel:
-    """definition of base model class"""
+    """definition of BaseModel class"""
 
     def __init__(self, *args, **kwargs):
-        """initializing the class"""
+        """initialization of the class"""
 
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
+                    setattr(self, key, datetime.strptime
+                            (value, time_format))
                 else:
                     setattr(self, key, value)
         else:
@@ -28,18 +30,18 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """return string representation of class"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                    self.id, self.__dict__)
+        """returns string representation of class"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """current time is set to updated_at"""
+        """updates updated_at to current time"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """dictonary containing all the keys and values"""
+        """returns dictionary containing all keys and values"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.strftime(time_format)
